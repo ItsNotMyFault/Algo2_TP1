c7dc55be7e42d1c619e9a0d117d5b82f7da57379
AlexisGuay <ItsNotMyFault_dev@Hotmail.com>
2020-09-19 23:38:02 -0400

    Reset des fichiers (reclone)

--- division.cpp
+++ division.cpp
@@ -1,32 +1,20 @@
 #include "division.hpp"
-#include <iostream>
+
 #include <assert.h>
 
-void division(const Polynome &dividende, // Le polynome qui est divise
-              const Polynome &diviseur,  // Le polynome qui divise
-              Polynome &quotient,       // Parametre de sortie: le resultat de la division
-              Polynome &reste) {       // Parametre de sortie: le reste de la division
-    assert(diviseur.degre() > 0 || diviseur.coefficient(0).numerateur() != 0); // Pas de division par zero
+void division(const Polynome& dividende, // Le polynome qui est divise
+	      const Polynome& diviseur,  // Le polynome qui divise
+	      Polynome& quotient,	   // Parametre de sortie: le resultat de la division
+	      Polynome& reste) {	   // Parametre de sortie: le reste de la division
+  assert(diviseur.degre() > 0 || diviseur.coefficient(0).numerateur() != 0); // Pas de division par zero
 
-    std::cout << "DIVISION ICI ========== " << std::endl;
-    std::cout << "  Entree Dividende: " << dividende << std::endl;
-    std::cout << "  Entree Dividende: " << diviseur << std::endl;
-    std::cout << "  Entree quotient: " << quotient << std::endl;
-    std::cout << "  Entree reste: " << reste << std::endl;
-
-    Rationnel coefficients_dividence[4] = {Rationnel(1), Rationnel(2), Rationnel(0), Rationnel(1)};
-    Polynome dividendeTest(std::vector<Rationnel>(coefficients_dividence, coefficients_dividence + 4)); // x^3 + 2x + 1
-    Polynome quotientTest(std::vector<Rationnel>(2, Rationnel(1))); // x + 1
-    Polynome resss = dividendeTest * quotientTest;
-    std::cout << "  resss: " << resss << std::endl;
-
-    // INSEREZ VOTRE CODE ICI
+  // INSEREZ VOTRE CODE ICI
 }
 
-Polynome plus_grand_commun_diviseur(const Polynome &a, const Polynome &b) {
-    assert(a >= b);
-    assert(a.degre() > 0 || a.coefficient(0).numerateur() != 0);
+Polynome plus_grand_commun_diviseur(const Polynome& a, const Polynome& b) {
+  assert(a >= b);
+  assert(a.degre() > 0 || a.coefficient(0).numerateur() != 0);
 
-    // INSEREZ VOTRE CODE ICI
-    return Polynome();
+  // INSEREZ VOTRE CODE ICI
+  return Polynome();
 }
--- division.hpp
+++ division.hpp
@@ -4,12 +4,11 @@
 #define DIVISION
 
 #include "polynome.hpp"
+void division(const Polynome& dividende, // Le polynome qui est divise
+	      const Polynome& diviseur,  // Le polynome qui divise
+	      Polynome& quotient,	   // Parametre de sortie: le resultat de la division
+	      Polynome& reste);	   // Parametre de sortie: le reste de la division
 
-void division(const Polynome &dividende, // Le polynome qui est divise
-              const Polynome &diviseur,  // Le polynome qui divise
-              Polynome &quotient,       // Parametre de sortie: le resultat de la division
-              Polynome &reste);       // Parametre de sortie: le reste de la division
-
-Polynome plus_grand_commun_diviseur(const Polynome &a, const Polynome &b);
+Polynome plus_grand_commun_diviseur(const Polynome& a, const Polynome& b);
 
 #endif
